3a9a069902966f65f369d3afc391b1b911624815
423april <423april@korea.ac.kr>
2022-05-17 16:00:31 +0900

    Update 03.py
    더 나은 풀이 방식(수정)

--- Greedy/03.py
+++ Greedy/03.py
@@ -1,7 +1,7 @@
 import sys
 n, m = map(int, input().split())
-lines = []
+max_val = 0
 for _ in range(n):
     line = list(map(int, sys.stdin.readline().split(" ")))
-    lines.append(min(line))
-print(max(lines))
+    max_val = max(max_val, min(line)) # 굳이 리스트에 넣어서 max 연산할 필요 없이, 매번 max 비교 해주면 max_val 구할 수 있다
+print(max_val)
