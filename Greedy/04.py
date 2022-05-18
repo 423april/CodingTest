22ef9eb13fe0ffe7dd704250b28bf61a3d76e2d9
423april <423april@korea.ac.kr>
2022-05-18 22:21:56 +0900

    Update 04.py

--- Greedy/04.py
+++ Greedy/04.py
@@ -1,5 +1,8 @@
 n, k = map(int, input().split(" "))
+n1, k1 = n, k
 count = 0
+
+# 내 풀이
 while n > 1:
     if n % k == 0:
         n = n // k
@@ -8,3 +11,16 @@ while n > 1:
         n = n - 1
         count += 1
 print(count)
+
+# 교재 풀이
+result = 0
+while True:
+    target = (n1//k1) * k1
+    result += n1 - target
+    n1 = target
+    if n1 < k1:
+        break
+    result += 1
+    n1 //= k1
+result += n1-1
+print(result)
